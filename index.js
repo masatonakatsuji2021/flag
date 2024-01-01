618b29f836259a1e8e88d83bb352d8d9005531fc
masato nakatsuji <nakatsuji.sak19@gmail.com>
2024-01-01 19:03:57 +0900

    1/1

--- index.js
+++ index.js
@@ -4,22 +4,26 @@ const Cli_1 = require("@flagfw/flag/bin/Cli");
 let args = Cli_1.default.getArgs();
 var mainCommand = args[0];
 if (mainCommand == "front") {
+    let front;
     try {
-        const front = require("@flagfw/front");
-        front.default();
+        front = require("@flagfw/front");
     }
     catch (err) {
         Cli_1.default.red("[ERROR] ").outn("Npm Module \"@flagfw/front\" is not installed or there is a problem with the module");
+        process.exit();
     }
+    front.default();
 }
 else if (mainCommand == "server") {
+    let server;
     try {
-        const server = require("@flagfw/server");
-        server.default();
+        server = require("@flagfw/server");
     }
     catch (err) {
         Cli_1.default.red("[ERROR] ").outn("Npm Module \"@flagfw/server\" is not installed or there is a problem with the module");
+        process.exit();
     }
+    server.default();
 }
 else {
     if (mainCommand) {
--- index.ts
+++ index.ts
@@ -4,20 +4,26 @@ let args = FlagCLI.getArgs();
 var mainCommand = args[0];
 
 if(mainCommand == "front"){
+    let front;
     try{
-        const front = require("@flagfw/front");
-        front.default();
+        front = require("@flagfw/front");
     }catch(err){
         FlagCLI.red("[ERROR] ").outn("Npm Module \"@flagfw/front\" is not installed or there is a problem with the module");
+        process.exit();
     }
+
+    front.default();
 }
 else if(mainCommand == "server"){
+    let server;
     try{
-        const server = require("@flagfw/server");
-        server.default();
+        server = require("@flagfw/server");
     }catch(err){
         FlagCLI.red("[ERROR] ").outn("Npm Module \"@flagfw/server\" is not installed or there is a problem with the module");
+        process.exit();
     }
+
+    server.default();
 }
 else{
     if(mainCommand){
